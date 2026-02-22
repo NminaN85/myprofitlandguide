4ea8d9ffb717c014ee24457454a842f13ca177b7
Mina <boles.minan@gmail.com>
2026-02-22 21:18:47 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -49,7 +49,7 @@ const products = {
         licenceUses: 1000,
         ingredients: {water: 0.15, electricity: 0.03 }
     },
-    Grapes: {
+    grapes: {
         workHours: 0.5,
         licenceUses: 450,
         ingredients: {water: 0.06, electricity: 0.02 }
